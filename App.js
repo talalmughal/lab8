aef55498840d3adca3c378d59a1ca9db6f42828b
talalmughal <68439311+talalmughal@users.noreply.github.com>
2020-11-28 21:55:09 +0500

    Task 1  Updated App.js for completion of task 1, added a header and Number input to get original price of the product, and the discount percentage that is to be applied on the price.

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TextInput } from "react-native";
 
 export default function App() {
+  const [Original_Price, SetPriceOriginal] = useState();
+  const [Discount_Percent, setPriceDiscount] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.headerStyles}> Calculator App (Discount)</Text>
+      <TextInput
+        style={styles.inputStyles}
+        placeholder="Enter Original Price"
+      />
+      <TextInput
+        style={[styles.inputStyles, { marginTop: 6 }]}
+        placeholder="Discount (%)"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "grey",
   },
+  headerStyles: {
+    textAlign: "center",
+    backgroundColor: "#63f542",
+    marginTop: 0,
+    marginBottom: 40,
+    padding: 5,
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#fff"
+  },
+  inputStyles: {
+    borderWidth: 1,
+    borderColor: "black",
+    width: "100%",
+    padding: 5,
+    margin: "auto",
+    borderRadius: 4
+  }
 });
